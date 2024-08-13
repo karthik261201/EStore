354459e0c1b82be4e4d74367f04a742b59641a47
karthik261201 <karthiknarayanan043@gmail.com>
2024-08-13 14:57:17 +0530

    added delivery address

--- src/app/home/components/cart/cart.component.ts
+++ src/app/home/components/cart/cart.component.ts
@@ -1,18 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
 import { CartStoreItem } from '../../services/cart/cart.storeitem';
 import { Router } from '@angular/router';
 import { CartItem } from '../../types/cart.type';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { loggedInUser } from '../../types/user.type';
+import { Subscription } from 'rxjs';
+import { UserService } from '../../services/users/user-service.service';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.scss']
 })
-export class CartComponent {
+export class CartComponent implements OnInit,OnDestroy {
   faTrash = faTrash;
+  orderForm: FormGroup;
+  user: loggedInUser;
+  subscriptions: Subscription = new Subscription();
 
-  constructor(public cartStore: CartStoreItem, private router: Router) {}
+  constructor(
+    public cartStore: CartStoreItem, 
+    private router: Router,
+    private fb: FormBuilder,
+    private userService: UserService
+  ) {
+    this.user = {
+      firstName: '',
+      lastName: '',
+      address: '',
+      city: '',
+      state: '',
+      pin:''
+    }
+
+    this.subscriptions.add(
+      userService.loggedInUser$.subscribe(loggedUser => {
+        if(loggedUser.firstName) {
+          this.user = loggedUser;
+        }
+      })
+    )
+  }
+
+  ngOnInit(): void {
+    this.orderForm = this.fb.group({
+      name: [`${this.user.firstName} ${this.user.lastName}`,Validators.required],
+      address: [`${this.user.address}`,Validators.required],
+      city: [`${this.user.city}`,Validators.required],
+      state: [`${this.user.state}`,Validators.required],
+      pin: [`${this.user.pin}`,Validators.required]
+    })
+  }
 
   navigateToHome() {
     this.router.navigate(['home/products'])
@@ -28,5 +67,11 @@ export class CartComponent {
 
   removeItem(cartItem: CartItem): void {
     this.cartStore.removeProduct(cartItem);
+  }
+
+  onSubmit() {}
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
   }
 }
